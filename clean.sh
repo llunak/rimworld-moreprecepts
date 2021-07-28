54bc1092ed293942746e74abc37f856c3f3403a8
Luboš Luňák <l.lunak@centrum.cz>
2021-07-28 15:51:03 +0200

    clean up also .git* files

--- clean.sh
+++ clean.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
+echo git status:
 git status --porcelain
 #wait for keypress or Ctrl+C
 read
 
 git clean -idx .
 rm -rf .git
+find . -type f -name '.git*' -print0 | xargs -0 rm -f
 rm -f clean.sh
