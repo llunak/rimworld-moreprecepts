e85c3be9a99270517fabe638793fec186d690d5b
Luboš Luňák <l.lunak@centrum.cz>
2021-08-26 14:51:01 +0200

    protect against (presumably) broken Pawns from mods

--- Source/PawnComp.cs
+++ Source/PawnComp.cs
@@ -22,62 +22,112 @@ namespace MorePrecepts
         public static int GetLastTakeAlcoholTick(Pawn pawn)
         {
             PawnComp comp = pawn.GetComp<PawnComp>();
+            if(comp == null)
+                return WarnBrokenPawn(pawn);
             return comp.lastTakeAlcoholTick;
         }
 
         public static void SetLastTakeAlcoholTickToNow(Pawn pawn)
         {
             PawnComp comp = pawn.GetComp<PawnComp>();
+            if(comp == null)
+            {
+                WarnBrokenPawn(pawn);
+                return;
+            }
             comp.lastTakeAlcoholTick = Find.TickManager.TicksGame;
         }
 
         public static int GetLastViolenceTick(Pawn pawn)
         {
             PawnComp comp = pawn.GetComp<PawnComp>();
+            if(comp == null)
+                return WarnBrokenPawn(pawn);
             return comp.lastViolenceTick;
         }
 
         public static void SetLastViolenceTickToNow(Pawn pawn)
         {
             PawnComp comp = pawn.GetComp<PawnComp>();
+            if(comp == null)
+            {
+                WarnBrokenPawn(pawn);
+                return;
+            }
             comp.lastViolenceTick = Find.TickManager.TicksGame;
         }
 
         public static void AddToLastViolenceTick(Pawn pawn, int add)
         {
             PawnComp comp = pawn.GetComp<PawnComp>();
+            if(comp == null)
+            {
+                WarnBrokenPawn(pawn);
+                return;
+            }
             comp.lastViolenceTick = Math.Min(comp.lastViolenceTick + add, Find.TickManager.TicksGame);
         }
 
         public static void SetBurnedOnPyre(Pawn pawn)
         {
             PawnComp comp = pawn.GetComp<PawnComp>();
+            if(comp == null)
+            {
+                WarnBrokenPawn(pawn);
+                return;
+            }
             comp.burnedOnPyre = true;
         }
 
         public static bool GetBurnedOnPyre(Pawn pawn)
         {
             PawnComp comp = pawn.GetComp<PawnComp>();
+            if(comp == null)
+            {
+                WarnBrokenPawn(pawn);
+                return false;
+            }
             return comp.burnedOnPyre;
         }
 
         public static int GetNoticedDrugsTick(Pawn pawn)
         {
             PawnComp comp = pawn.GetComp<PawnComp>();
+            if(comp == null)
+                return WarnBrokenPawn(pawn);
             return comp.noticedDrugsTick;
         }
 
         public static void SetNoticedDrugsTick(Pawn pawn, int tick)
         {
             PawnComp comp = pawn.GetComp<PawnComp>();
+            if(comp == null)
+            {
+                WarnBrokenPawn(pawn);
+                return;
+            }
             comp.noticedDrugsTick = tick;
         }
 
         public static void SetNoticedDrugsTickIfNotSet(Pawn pawn, int tick)
         {
             PawnComp comp = pawn.GetComp<PawnComp>();
+            if(comp == null)
+            {
+                WarnBrokenPawn(pawn);
+                return;
+            }
             if(comp.noticedDrugsTick < 0)
                 comp.noticedDrugsTick = tick;
+        }
+
+        private static int WarnBrokenPawn(Pawn pawn)
+        {
+            // We patch the BasePawn ThingDef, so the PawnComp should always be there.
+            // If not, then unless proven otherwise assume a mod that creates pawns without basing
+            // them on the BasePawn ThingDef, and just ignore them.
+            Log.Warning("Pawn " + pawn + " lacks MorePrecepts.PawnComp, not based on BasePawn ThingDef?");
+            return -99999;
         }
 
         public override void Initialize(CompProperties props)
