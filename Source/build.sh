d105a2f42e9dd8fad7902bd257b57ec84292ab3e
Luboš Luňák <l.lunak@centrum.cz>
2022-11-17 10:41:53 +0100

    remove System.*.dll binaries that get created for some reason
    This is a hack, but I have no idea where it comes from and it breaks
    the game.

--- Source/build.sh
+++ Source/build.sh
@@ -1,2 +1,6 @@
 #! /bin/sh
 FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.7.2-api/ dotnet build MorePrecepts.csproj /property:Configuration=Release
+if test $? -eq 0; then
+    # no idea why these get created, but they break game loading
+    rm ../1.4/Assemblies/System.*.dll
+fi
