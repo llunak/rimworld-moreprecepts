eb6dc62e7eb5ab5412cb57b1661319d226cc540f
Luboš Luňák <l.lunak@centrum.cz>
2022-11-22 08:15:09 +0100

    update hack to remove more unwanted dll's

--- Source/build.sh
+++ Source/build.sh
@@ -2,5 +2,5 @@
 FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.7.2-api/ dotnet build MorePrecepts.csproj /property:Configuration=Release
 if test $? -eq 0; then
     # no idea why these get created, but they break game loading
-    rm ../1.4/Assemblies/System.*.dll
+    rm -f ../Assemblies/System*.dll ../Assemblies/mscorlib*.dll
 fi
