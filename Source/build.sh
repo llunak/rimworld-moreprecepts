b42de66c0f9cf9461eac6c864b77a03f89d725d5
Luboš Luňák <l.lunak@centrum.cz>
2024-07-21 13:12:07 +0200

    make the build script more generic

--- Source/build.sh
+++ Source/build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.7.2-api/ dotnet build MorePrecepts.csproj /property:Configuration=Release
+FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.7.2-api/ dotnet build *.csproj /property:Configuration=Release
 if test $? -eq 0; then
     # no idea why these get created, but they break game loading
     rm -f ../Assemblies/System*.dll ../Assemblies/mscorlib*.dll
