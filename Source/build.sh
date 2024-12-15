9b020a56d3eecdde36db1b915bd28aadc12de21e
Luboš Luňák <l.lunak@centrum.cz>
2024-12-15 13:08:52 +0100

    new version of build.sh script

--- Source/build.sh
+++ Source/build.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
-name=MorePrecepts
+name=$(ls *.csproj | sed 's/.csproj//')
 rm -f ../Assemblies/$name.dll 2>/dev/null
-FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.7.2-api/ DOTNET_CLI_TELEMETRY_OPTOUT=1 dotnet build $name.csproj /property:Configuration=Release
-if test $? -eq 0; then
-    # no idea why these get created, but they break game loading
-    shopt -s extglob
-    rm -f $(ls -1 ../Assemblies/*.dll | grep -v $name)
-fi
+DOTNET_CLI_TELEMETRY_OPTOUT=1 dotnet build $name.csproj /property:Configuration=Release
