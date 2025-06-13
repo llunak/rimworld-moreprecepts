1fba83bb5a5c03f8e7202495bf1cda4f3e6634b3
Luboš Luňák <l.lunak@centrum.cz>
2025-06-13 07:54:13 +0200

    update JobDriver_SitAndBeSociallyActive
    JobDriver_StandAndBeSociallyActive can maintain direction now.

--- Source/Utils.cs
+++ Source/Utils.cs
@@ -16,30 +16,12 @@ namespace MorePrecepts
         }
     }
 
-    // Like JobDriver_StandAndBeSociallyActive, but also reserve the seat, and don't
-    // turn towards other pawns (=keep facing the table).
-    public class JobDriver_SitAndBeSociallyActive : JobDriver
+    // Like JobDriver_StandAndBeSociallyActive, but also reserve the seat.
+    public class JobDriver_SitAndBeSociallyActive : JobDriver_StandAndBeSociallyActive
     {
         public override bool TryMakePreToilReservations(bool errorOnFailed)
         {
             return pawn.ReserveSittableOrSpot(pawn.Position, job, errorOnFailed);
-        }
-
-        public override IEnumerable<Toil> MakeNewToils()
-        {
-            if (base.TargetLocA.IsValid)
-                yield return Toils_Goto.GotoCell(base.TargetLocA, PathEndMode.OnCell);
-            Toil toil = new Toil();
-            toil.tickIntervalAction = delegate(int delta)
-            {
-                if (job.lookDirection != Direction8Way.Invalid)
-                    base.pawn.rotationTracker.Face(base.pawn.Position.ToVector3() + job.lookDirection.AsVector());
-                base.pawn.GainComfortFromCellIfPossible(delta);
-            };
-            toil.socialMode = RandomSocialMode.SuperActive;
-            toil.defaultCompleteMode = ToilCompleteMode.Never;
-            toil.handlingFacing = true;
-            yield return toil;
         }
     }
 
