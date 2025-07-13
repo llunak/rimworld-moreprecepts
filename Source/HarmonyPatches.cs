ec63e4ac46df5b3c08f6a07695a4e911a8cf97d6
Luboš Luňák <l.lunak@centrum.cz>
2025-07-13 10:55:58 +0200

    remove patching of CommonSense
    I've sent a PR to the author to remove the copy&paste.

--- Source/HarmonyPatches.cs
+++ Source/HarmonyPatches.cs
@@ -23,7 +23,7 @@ namespace MorePrecepts
             {
                 oldMethod = AccessTools.Method(nestedClass, "<BestFoodSourceOnMap>b__0");
                 newMethod = typeof(FoodUtility_Patch).GetMethod("BestFoodSourceOnMap_foodValidator");
-                if(oldMethod != null)
+                if(oldMethod != null && newMethod != null)
                 {
                     harmony.Patch(oldMethod, transpiler: new HarmonyMethod(newMethod));
                     done = true;
@@ -39,7 +39,7 @@ namespace MorePrecepts
             {
                 oldMethod = AccessTools.Method(nestedClass, "<TryFindIngestibleToNurse>g__Validator|0");
                 newMethod = typeof(JoyGiver_SocialRelax_Patch).GetMethod("TryFindIngestibleToNurse_validator");
-                if(oldMethod != null)
+                if(oldMethod != null && newMethod != null)
                 {
                     harmony.Patch(oldMethod, transpiler: new HarmonyMethod(newMethod));
                     done = true;
@@ -48,32 +48,11 @@ namespace MorePrecepts
             if(!done)
                 Log.Error("MorePrecepts: Failed to find TryFindIngestibleToNurse_validator for patching");
 
-// TODO
-            // CommonSense mod copy&pastes the Toils_Ingest.CarryIngestibleToChewSpot() code.
-            done = false;
-            Type commonSense = AccessTools.TypeByName("CommonSense.JobDriver_PrepareToIngestToils_ToolUser_CommonSensePatch");
-            if( commonSense != null )
-            {
-                nestedClass = commonSense.GetNestedType("<>c__DisplayClass7_0", BindingFlags.NonPublic);
-                if(nestedClass != null)
-                {
-                    oldMethod = AccessTools.Method(nestedClass, "<ReserveChewSpot>b__0");
-                    newMethod = typeof(Toils_Ingest_Patch).GetMethod("CarryIngestibleToChewSpot_delegate");
-                    if(oldMethod != null)
-                    {
-                        harmony.Patch(oldMethod, transpiler: new HarmonyMethod(newMethod));
-                        done = true;
-                    }
-                }
-                if(!done)
-                    Log.Error("MorePrecepts: Failed to find CommonSense.ReserveChewSpot delegate for patching");
-            }
-
             // JobDriver_Strip.MakeNewToils() needs special handling, see the transpiller.
             done = false;
             oldMethod = AccessTools.Method(typeof(JobDriver_Strip), "<MakeNewToils>b__2_2");
             newMethod = typeof(JobDriver_Strip_Patch).GetMethod("MakeNewToils_delegate");
-            if(oldMethod != null)
+            if(oldMethod != null && newMethod != null)
             {
                 harmony.Patch(oldMethod, transpiler: new HarmonyMethod(newMethod));
                 done = true;
